ca78035bd2f8cef415e2c0c13cbbb605d6104ed9
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2022-02-18 00:28:51 -0500

    added headquarters marker with geocode function

--- js/harrisMap.js
+++ js/harrisMap.js
@@ -10,14 +10,33 @@ $(document).ready(function () {
         {
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v11',
-            center: [-83.37747694511727, 33.959023510866395],
+            center: [-75.72560527974049, 45.75144829393112],
             zoom: 15
         }
     )
+//corporate home marker - geocode
+    geocode('1 Antares Dr., Suite 400\n' +
+        'Ottawa ON, K2E 8C4', mapboxApiKey).then(function (results){
+        var canada = new mapboxgl.Marker({
+            color: '#5097a5'
+        })
+            .setLngLat(results)
+            .addTo(map)
+
+        new mapboxgl.Popup()
+            .setLngLat(results)
+            .setHTML('<h3><em>Harris Computer Systems</em></h3>')
+            .addTo(map)
+
+        // marker.setPopup(canada)
+    })
+
+
+
 
     //place a zoom and rotate
     var zoomWheel = map.scrollZoom.setZoomRate(1 / 25);
-    var rotate = map.dragRotate
+    var rotate = map.dragRotate.enable();
 
     //marker
     var marker = new mapboxgl.Marker({
@@ -31,5 +50,10 @@ $(document).ready(function () {
         .setLngLat([-83.37747694511727, 33.959023510866395])
         .setHTML('<h1 class="pop"><em>Harris of Athens</em></h1>')
         .addTo(map)
+
+    marker.setPopup(harrisPopUp);
+
+    // fly to
+    map.flyTo({center: [-83.37747694511727, 33.959023510866395]})
 
 })
